cbf209c953cf32162c5d528a1d6292f355bf00e1
kingcrud12 <dipitay@gmail.com>
2026-03-06 15:29:51 +0100

    update repo

---
+++ Backend/API/pmt_api/src/main/java/fr/techcrud/pmt_api/PmtApiApplication.java
@@ -0,0 +1,13 @@
+package fr.techcrud.pmt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmtApiApplication.class, args);
+	}
+
+}
---
+++ Backend/API/pmt_api/src/test/java/fr/techcrud/pmt_api/PmtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.techcrud.pmt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
